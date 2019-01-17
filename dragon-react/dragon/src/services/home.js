84bbb770b0ef9c354ee81ea3693c5a29f688b5c8
truongnguyen111995 <nguyennhattruong111995@gmail.com>
2019-01-17 22:20:46 +0700

    fix util

--- dragon-react/dragon/src/services/home.js
+++ dragon-react/dragon/src/services/home.js
@@ -17,7 +17,7 @@ const HomeService = {
     getDetailHome: async function (homeId) {
         try {
             const response = await getFromUrl(
-                "http://localhost:8080/dragons/room/by_home_id/?homeId="+homeId);
+                "/dragons/room/by_home_id/?homeId="+homeId);
             const data = response.data;
             return data
             
@@ -30,7 +30,7 @@ const HomeService = {
     getDetailRoom: async function (roomId) {
         try {
             const response = await getFromUrl(
-                "http://localhost:8080/dragons/room_detail/by_room_id?roomId="+roomId);
+                "/dragons/room_detail/by_room_id?roomId="+roomId);
             const data = response.data;
             return data
             
