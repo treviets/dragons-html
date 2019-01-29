5b70f79e0a323ce472507d2fdc34291df4ba2e17
truongnguyen111995 <nguyennhattruong111995@gmail.com>
2019-01-29 16:14:21 +0700

    footer

--- dragon-react/dragon/src/containers/PageFooter.js
+++ dragon-react/dragon/src/containers/PageFooter.js
@@ -7,30 +7,30 @@ class PageFooter extends Component{
             <br/>
             <div className="container">
                 <div className="footer-custom">
-                    <div className="row">
+                    <div className="row font-size14">
                         <div className="col-md-3 col-sm-12">
                             <div className="box">
                                 <ul>
-                                    <li className="title-footer">Airbnb</li>
-                                    <li><a href="">Careers</a></li>
-                                    <li><a href="">Press</a></li>
-                                    <li><a href="">Policies</a></li>
-                                    <li><a href="">Help</a></li>
-                                    <li><a href="">Diversity & Belonging</a></li>
+                                    <li className="font-title title-footer">Airbnb</li>
+                                    <li className="font-medium"><a href="">Careers</a></li>
+                                    <li className="font-medium"><a href="">Press</a></li>
+                                    <li className="font-medium"><a href="">Policies</a></li>
+                                    <li className="font-medium"><a href="">Help</a></li>
+                                    <li className="font-medium"><a href="">Diversity & Belonging</a></li>
                                 </ul>
                             </div>
                         </div>
                         <div className="col-md-3 col-sm-12">
                             <div className="box">
                                 <ul>
-                                    <li className="title-footer">Discover</li>
-                                    <li><a href="">Trust & Safety</a></li>
-                                    <li><a href="">Travel Credit</a></li>
-                                    <li><a href="">Gift Cards</a></li>
-                                    <li><a href="">Airbnb Citizen</a></li>
-                                    <li><a href="">Guidebooks</a></li>
-                                    <li><a href="">Airbnbmag</a></li>
-                                    <li>
+                                    <li className="font-title title-footer">Discover</li>
+                                    <li className="font-medium"><a href="">Trust & Safety</a></li>
+                                    <li className="font-medium"><a href="">Travel Credit</a></li>
+                                    <li className="font-medium"><a href="">Gift Cards</a></li>
+                                    <li className="font-medium"><a href="">Airbnb Citizen</a></li>
+                                    <li className="font-medium"><a href="">Guidebooks</a></li>
+                                    <li className="font-medium"><a href="">Airbnbmag</a></li>
+                                    <li className="font-medium">
                                         <a href="">EventsNew</a>
                                         <span className="_146g3kk"><span className="_4btwil"><span className="_1awrjnc1">New</span></span></span>
                                     </li>
@@ -40,16 +40,16 @@ class PageFooter extends Component{
                         <div className="col-md-3 col-sm-12">
                             <div className="box">
                                 <ul>
-                                    <li className="title-footer">Hosting</li>
-                                    <li><a href="">Why Host</a></li>
-                                    <li><a href="">Hospitality</a></li>
-                                    <li><a href="">Responsible Hosting</a></li>
-                                    <li><a href="">Community Center</a></li>
-                                    <li>
+                                    <li className="font-title title-footer">Hosting</li>
+                                    <li className="font-medium"><a href="">Why Host</a></li>
+                                    <li className="font-medium"><a href="">Hospitality</a></li>
+                                    <li className="font-medium"><a href="">Responsible Hosting</a></li>
+                                    <li className="font-medium"><a href="">Community Center</a></li>
+                                    <li className="font-medium">
                                         <a href="">Host an ExperienceNew</a>
                                         <span className="_146g3kk"><span className="_4btwil"><span className="_1awrjnc1">New</span></span></span>
                                     </li>
-                                    <li>
+                                    <li className="font-medium">
                                         <a href="">Open HomesNew</a>
                                         <span className="_146g3kk"><span className="_4btwil"><span className="_1awrjnc1">New</span></span></span>
                                     </li>
@@ -59,14 +59,14 @@ class PageFooter extends Component{
                         <div className="col-md-3 col-sm-12">
                             <div className="box">
                                 <ul>
-                                    <li className="title-footer">
-                                        <i className="fa fa-facebook" aria-hidden="true"></i>
-                                        <i className="fa fa-twitter" aria-hidden="true"></i>
-                                        <i className="fa fa-instagram" aria-hidden="true"></i>
+                                    <li className="title-footer title-footer" style={{webkitTextFillColor:'rgb(118, 118, 118)'}}>
+                                        <i className="fa fa-facebook fa-lg" fill aria-hidden="true"></i>
+                                        <i className="fa fa-twitter fa-lg margin-left15" aria-hidden="true"></i>
+                                        <i className="fa fa-instagram fa-lg margin-left15" aria-hidden="true"></i>
                                     </li>
-                                    <li><a href="">Terms</a></li>
-                                    <li><a href="">Privacy</a></li>
-                                    <li>
+                                    <li className="font-medium"><a href="">Terms</a></li>
+                                    <li className="font-medium"><a href="">Privacy</a></li>
+                                    <li className="font-medium">
                                         <a href="">Site Map</a>
                                         <span className="_146g3kk"><span className="_4btwil"><span className="_1awrjnc1">New</span></span></span>
                                     </li>
